2cfa316cd4fd8ea85351b6ca59d48c9b6062d1c9
Erhan Cagirici <erhan@upbound.io>
2026-04-23 10:42:25 +0300

    quicksight: configure remaining sdk resources
    Signed-off-by: Erhan Cagirici <erhan@upbound.io>

--- config/externalname.go
+++ config/externalname.go
@@ -2113,24 +2113,30 @@ var TerraformPluginSDKExternalNameConfigs = map[string]config.ExternalName{
 
 	// quicksight
 	//
+	// QuickSight Account Subscription can be imported using aws_account_id
+	"aws_quicksight_account_subscription": config.ParameterAsIdentifier("aws_account_id"),
+	// QuickSight Analysis can be imported using the aws account id and the analysis id separated by a comma
+	"aws_quicksight_analysis": FormattedIdentifierFromProvider(",", "aws_account_id", "analysis_id"),
+	// QuickSight Dashboard can be imported using the aws account id and the dashboard id separated by a comma
+	"aws_quicksight_dashboard": FormattedIdentifierFromProvider(",", "aws_account_id", "dashboard_id"),
+	// QuickSight Data Set can be imported using the aws account id and the data set id separated by a comma
+	"aws_quicksight_data_set": FormattedIdentifierFromProvider(",", "aws_account_id", "data_set_id"),
+	// QuickSight Data Source can be imported using the aws account id and the data source id separated by /
+	"aws_quicksight_data_source": FormattedIdentifierFromProvider("/", "aws_account_id", "data_source_id"),
+	// QuickSight Theme can be imported using the aws account id and the template id separated by a comma
+	"aws_quicksight_folder": FormattedIdentifierFromProvider(",", "aws_account_id", "folder_id"),
 	// QuickSight Group can be imported using the aws account id, namespace and group name separated by /
 	// 123456789123/default/tf-example
 	"aws_quicksight_group": FormattedIdentifierFromProvider("/", "aws_account_id", "namespace", "group_name"),
+	// QuickSight Group Membership can be imported using the aws account id, namespace, group name and member name separated by /
+	"aws_quicksight_group_membership": FormattedIdentifierFromProvider("/", "aws_account_id", "namespace", "group_name", "member_name"),
+	// QuickSight Template can be imported using the aws account id and the template id separated by a comma
+	"aws_quicksight_template": FormattedIdentifierFromProvider(",", "aws_account_id", "template_id"),
+	// QuickSight Theme can be imported using the aws account id and the template id separated by a comma
+	"aws_quicksight_theme": FormattedIdentifierFromProvider(",", "aws_account_id", "theme_id"),
 	// No import
 	// TODO: For now API is not normalized. While testing resource we can check the actual ID and normalize the API.
 	"aws_quicksight_user": config.IdentifierFromProvider,
-	// QuickSight Account Subscription can be imported using aws_account_id
-	"aws_quicksight_account_subscription": config.ParameterAsIdentifier("aws_account_id"),
-	// QuickSight Data Source can be imported using the aws account id and the data source id separated by /
-	"aws_quicksight_data_source": FormattedIdentifierFromProvider("/", "aws_account_id", "data_source_id"),
-	// QuickSight Data Set can be imported using the aws account id and the data set id separated by a comma
-	"aws_quicksight_data_set": FormattedIdentifierFromProvider(",", "aws_account_id", "data_set_id"),
-	// QuickSight Dashboard can be imported using the aws account id and the dashboard id separated by a comma
-	"aws_quicksight_dashboard": FormattedIdentifierFromProvider(",", "aws_account_id", "dashboard_id"),
-	// QuickSight Analysis can be imported using the aws account id and the analysis id separated by a comma
-	"aws_quicksight_analysis": FormattedIdentifierFromProvider(",", "aws_account_id", "analysis_id"),
-	// QuickSight Group Membership can be imported using the aws account id, namespace, group name and member name separated by /
-	"aws_quicksight_group_membership": FormattedIdentifierFromProvider("/", "aws_account_id", "namespace", "group_name", "member_name"),
 
 	// ram
 	//
